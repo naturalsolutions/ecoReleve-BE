117db20c3bb24bf94ea802ed7ee7a284b2ea48fc
jvitus <jeanvitus.albertini@gmail.com>
2018-06-11 17:06:03 +0200

    handle abspath for conf

--- Back/ecoreleve_be_server/Models/__init__.py
+++ Back/ecoreleve_be_server/Models/__init__.py
@@ -4,9 +4,12 @@ from sqlalchemy import select
 from sqlalchemy.exc import TimeoutError
 import pandas as pd
 from traceback import print_exc
-
+import os
 AppConfig = configparser.ConfigParser()
-AppConfig.read('././development.ini')
+
+absPath = os.path.abspath( os.path.join(os.path.dirname(__file__),'../../development.ini'))
+AppConfig.read(absPath)
+# AppConfig.read('././development.ini')
 print(AppConfig['app:main']['sensor_schema'])
 
 pendingSensorData = []
