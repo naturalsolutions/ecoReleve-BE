836a294183be5c417a63e77c3a3748ec01192282
jvitus <jeanvitus.albertini@gmail.com>
2018-09-05 15:47:27 +0200

    superUser have all permissions now

--- Back/ecoreleve_be_server/controllers/security.py
+++ Back/ecoreleve_be_server/controllers/security.py
@@ -170,14 +170,14 @@ context_permissions = {
 
     'stations': [
         (Allow, 'group:admin', ALL_PERMISSIONS),
-        (Allow, 'group:superUser', ('create', 'update', 'read')),
+        (Allow, 'group:superUser', ALL_PERMISSIONS),
         (Allow, 'group:user', ('create', 'update', 'read')),
         (Deny, 'group:client', ALL_PERMISSIONS)
     ],
 
     'observations': [
         (Allow, 'group:admin', ALL_PERMISSIONS),
-        (Allow, 'group:superUser', ('create', 'update', 'read')),
+        (Allow, 'group:superUser', ALL_PERMISSIONS),
         (Allow, 'group:user', ALL_PERMISSIONS),
         (Deny, 'group:client', ALL_PERMISSIONS)
         
@@ -217,7 +217,7 @@ context_permissions = {
 
     'projects': [
         (Allow, 'group:admin', ALL_PERMISSIONS),
-        (Allow, 'group:superUser', ('create', 'update', 'read')),
+        (Allow, 'group:superUser', ALL_PERMISSIONS),
         (Allow, 'group:user', 'read'),
         (Allow, 'group:client', 'read')
         
@@ -225,7 +225,7 @@ context_permissions = {
 
     'project': [
         (Allow, 'group:admin', ALL_PERMISSIONS),
-        (Allow, 'group:superUser', ('create', 'update', 'read')),
+        (Allow, 'group:superUser', ALL_PERMISSIONS),
         (Allow, 'group:user', 'read'),
         (Deny, 'group:client', ALL_PERMISSIONS)
         
@@ -233,7 +233,7 @@ context_permissions = {
 
     'clients': [
         (Allow, 'group:admin', ALL_PERMISSIONS),
-        (Allow, 'group:superUser', ('create', 'update', 'read')),
+        (Allow, 'group:superUser', ALL_PERMISSIONS),
         (Allow, 'group:user', 'read'),
         (Deny, 'group:client', ALL_PERMISSIONS)
         
